9c0bb71fc05c2e44560475f5bc26b357ee2fd9d6
Jose Veliz <jose.veliz.francia@gmail.com>
2018-09-24 14:00:08 -0500

    Bottom Navigation view Ready Principal Fragments ready

--- app/src/main/java/com/zetagh/clanbattleapp/viewcontrollers/activities/MainActivity.kt
+++ app/src/main/java/com/zetagh/clanbattleapp/viewcontrollers/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zetagh.clanbattleapp.viewcontrollers.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.zetagh.clanbattleapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,5 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        toolbar.title = "Home in case"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_toolbar,menu)
+        return true
     }
 }
